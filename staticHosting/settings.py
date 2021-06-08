3472c9f3f3e62464e50293b1c6fa89ddc8c8b88b
Aldin Philo <aldinphilo@gmail.com>
2021-06-09 00:24:11 +0530

    setting debug false

--- staticHosting/settings.py
+++ staticHosting/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l%ujwj5=b*-bi&@^0y&obgtqf%x!5fxzlkh0+!vhwmuf5agifm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
